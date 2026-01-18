b2a3163d639ef76d7fed6867747e7315e173f196
hamzoffcl <hamzzofficial10@gmail.com>
2026-01-18 14:09:47 +0800

    Update IdChannel.js

--- IdChannel.js
+++ IdChannel.js
@@ -1,1 +1,51 @@
-
+[
+  {
+    "id": "120363422260832658@newsletter",
+    "desc": "testi hamz"
+  },
+  {
+    "id": "120363403321497277@newsletter",
+    "desc": "ch utama"
+  },
+  {
+    "id": "120363405793184505@newsletter",
+    "desc": "ch market 1"
+  },
+  {
+    "id": "120363423788079174@newsletter",
+    "desc": "ch market 2"
+  },
+  {
+    "id": "120363424915023399@newsletter
+",
+    "desc": "ch market 3"
+  },
+  {
+    "id": "120363423163722091@newsletter",
+    "desc": "ch gabut"
+  },
+  {
+    "id": "120363424856571704@newsletter",
+    "desc": "ch matz"
+  },
+  {
+    "id": "120363419298914034@newsletter",
+    "desc": "ch hamzy"
+  },
+  {
+    "id": "120363404833500925@newsletter",
+    "desc": "ch xinn"
+  },
+  {
+    "id": "120363402827950476@newsletter",
+    "desc": "ch romy"
+  },
+  {
+    "id": "120363423374324654@newsletter",
+    "desc": "ch san"
+  },
+  {
+    "id": "120363425314992907@newsletter",
+    "desc": "ch zar"
+  }
+]
